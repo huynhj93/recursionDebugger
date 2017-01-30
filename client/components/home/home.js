7954722c8ec44bf49f1f254f6867b863cbbdaa85
John Huynh <huynh.j93@gmail.com>
2017-01-30 00:06:14 -0800

    [add]-Finished Final Code Transform

--- client/components/home/home.js
+++ client/components/home/home.js
@@ -1,8 +1,8 @@
 import React  from 'react';
 import CodeMirror from 'react-codemirror';
+// have to deploy to see if this works 
 import mode from '../../../node_modules/codemirror/mode/javascript/javascript.js';
 import recast from 'recast';
-import parseFunc from '../../../lib/parseFunction';
 class Home extends React.Component {
 	constructor (props) {
 		super(props);
@@ -15,18 +15,9 @@ class Home extends React.Component {
 		this.logState = this.logState.bind(this);
 		this.updateCode = this.updateCode.bind(this);
 		this.transformCode = this.transformCode.bind(this);
+		this.getRecursionInvokedLine = this.getRecursionInvokedLine.bind(this);
 		this.getRecursionCallLine = this.getRecursionCallLine.bind(this);
 		this.getFunctionDeclaration = this.getFunctionDeclaration.bind(this);
-	};
-
-	componentWillMount(){
-		// var s = "function test(){  alert(1); }";
-
-		// //"Register" the function
-		// eval(s);
-
-		//Call the function
-		// test();
 	};
 
 	logState () {
@@ -49,7 +40,9 @@ class Home extends React.Component {
 	getRecursionCallLine (event) {
 		this.setState({cLineNum: Number(event.target.value)});
 	};
-
+	getRecursionInvokedLine (event) {
+		this.setState({iLineNum: Number(event.target.value)});
+	};
 	getWhiteSpace (string) {
 		var res = '';
 		for (var i = 0; i < string.length; i++) {
@@ -62,15 +55,49 @@ class Home extends React.Component {
 		return res;
 	};
 
+	getParams (arr) {
+		const res = [];
+		for (let i = arr.length - 1; i >= 0; i--) {
+			if (arr[i] === '(') {
+				break;
+			}
+			res.push(arr[i])
+		}
+		const revArr = [];
+		for (let i = res.length - 1; i >= 0; i--) {
+			revArr.push(res[i]);
+		}
+		const params = [];
+		var param = '';
+		for (let i = 0; i < revArr.length; i++) {
+			if (revArr[i] === ',') {
+				params.push(param);
+				param = '';
+			} else if (i === revArr.length -1) {
+				param += revArr[i];
+				params.push(param);
+			} else if (revArr[i] === ' ') {
+				continue;
+			} else {
+				param += revArr[i];
+			}
+		}
+ 		return params;
+	};
+
 	transformCode (e) {
 		e.preventDefault();
 		const zeroShiftF = this.state.fLineNum - 1;
 		const zeroShiftC = this.state.cLineNum - 1;
 		const fDeclaration = this.state.codeArr[zeroShiftF];
 		const fDeclarationArr = fDeclaration.split('');
-		fDeclarationArr.pop();
-		fDeclarationArr.pop();
-		const stackParam = ', stack){';
+		while (fDeclarationArr[fDeclarationArr.length - 1] === '{' || fDeclarationArr[fDeclarationArr.length - 1] === ')' || fDeclarationArr[fDeclarationArr.length - 1] === ' ') {
+			fDeclarationArr.pop();
+		}
+		console.log(fDeclarationArr.join(''));
+		const params = this.getParams(fDeclarationArr);
+		console.log('params is:', params);
+		const stackParam = ', stack, state){';
 		const stackArr = stackParam.split('');
 		const transformFDeclaration = fDeclarationArr.concat(stackArr);
 		const newCodeArr = this.state.codeArr;
@@ -86,14 +113,37 @@ class Home extends React.Component {
 		const afterRecurseCall = this.state.codeArr.slice(zeroShiftC);
 		afterRecurseCall.splice(1, 0, decrementStack);
 		const transformCCode = beforeRecurseCall.concat(afterRecurseCall);
-		var stringTransform = transformCCode.join('');
+		const stringCTranform = transformCCode.join('');
+		const beforeRecurseDeclaration = transformCCode.slice(0, zeroShiftF + 1);
+		const afterRecurseDeclaration = transformCCode.slice(zeroShiftF + 1);
+		console.log(beforeRecurseDeclaration);
+		console.log(afterRecurseDeclaration);
+		const stackInsert = whiteSpace + 'var callStack = {};';
+		const stackDefine1 = whiteSpace + 'for (var q = 0; q < arguments.length; q++) {';
+		const stackDefine2 = whiteSpace + '	callStack[arguments[q]] = arguments[q];';
+		const stackDefine3 = whiteSpace + '}';
+		const stackDefine4 = whiteSpace + 'callStack["stack"] = stack;';
+		const pushStack = whiteSpace + 'state.callStack.push(callStack);';
+		console.log(stackInsert);
+		console.log(stackDefine1);
+		console.log(pushStack);
+		beforeRecurseDeclaration.push(stackInsert);
+		beforeRecurseDeclaration.push(stackDefine1);
+		beforeRecurseDeclaration.push(stackDefine2);
+		beforeRecurseDeclaration.push(stackDefine3);
+		beforeRecurseDeclaration.push(stackDefine4);
+		beforeRecurseDeclaration.push(pushStack);
+		console.log(beforeRecurseDeclaration);
+		const finalTransform = beforeRecurseDeclaration.concat(afterRecurseDeclaration);
+		console.log(finalTransform.join('\n'));
+		// beforeRecurseDeclaration.push(whiteSpace + 'state.callStack.push(params[i])');
 		//using eval did not work for some reason
-		// eval(stringTransform);
-		var newCode = document.createElement('script');
-		newCode.text = stringTransform;
+		// eval(stringCTranform);
+		const newCode = document.createElement('script');
+		newCode.text = stringCTranform;
 		document.getElementsByTagName('head')[0].appendChild(newCode);
 		console.log(factorial(5));
-		this.setState({codeArr: newCodeArr});
+		this.setState({codeArr: transformCCode});
 	};
 
 	render () {
@@ -106,7 +156,8 @@ class Home extends React.Component {
 				<CodeMirror value={this.state.code} onChange={this.updateCode} options={options} />
 				<form onSubmit={this.transformCode}>
 					<input onChange={this.getFunctionDeclaration} type='text' placeholder='What line do you declare your function recurse?' required></input>
-					<input onChange={this.getRecursionCallLine} type='text' placeholder='What line do you call recursion on?' required></input>
+					<input onChange={this.getRecursionCallLine} type='text' placeholder='What line in your recursion function do you call recurse on?' required></input>
+					<input onChange={this.getRecursionInvokedLine} type='text' placeholder='What line do you initiate your recurse function?' required></input>
 					<button type='submit'>Submit</button>
 				</form>
 			</div>
